3a4cbea34eb738fc8a83dfba2c9b735ee563ae63
Navya Bharathan <navyabharathan@Navyas-MacBook-Pro.local>
2022-11-01 12:55:03 +0100

    used cloudinary

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require('./db');
 const express=require('express');
 
 
+
 const {userRouter}=require("./routes/user");
 const {propertyRouter}=require("./routes/property");
 const {authRouter}=require("./routes/auth");
 const {errorHandler}=require("./utils/errorHandler")
 const cors =require('cors');
+const cloudinary=require("./cloudinary/cloudinary");
 
 require('./controllers/user');
 require('./controllers/property');
@@ -19,7 +21,8 @@ const app=express();
 const port=process.env.PORT || 3200;
 
 //middlewares
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({extended:true,limit:'50mb'}))  //check1
 app.use(cors());
 
 app.get("/",(req,res)=>{
